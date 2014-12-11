4b916be331f829b4c2a21242eadbfb5e333d57ef
oihane <oihanecruce@gmail.com>
2014-12-11 09:42:13 +0100

    [ADD] Moved <mrp_production_notes>, <product_packaging_views> & <stock_lot_notes>

--- product_packaging_views/__openerp__.py
+++ product_packaging_views/__openerp__.py
@@ -2,14 +2,14 @@
 ##############################################################################
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see http://www.gnu.org/licenses/.
@@ -17,21 +17,24 @@
 ##############################################################################
 
 {
-    "name": "MRP BoM Notes",
+    "name": "Product Packaging extended views",
     "version": "1.0",
+    "depends": [
+        "product",
+    ],
     "author": "OdooMRP team",
-    "website": "http://www.odoomrp.com",
     "contributors": [
         "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
-        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
-        "Ana Juaristi <ajuaristio@gmail.com>"
     ],
-    "category": "Manufacturing",
-    "depends": [
-        "mrp",
-    ],
+    "category": "Stock management",
+    "website": "http://www.odoomrp.com",
+    "complexity": "normal",
+    "summary": "",
+    "description": """
+    """,
     "data": [
-        "views/mrp_bom_view.xml",
+        "views/product_view.xml",
     ],
-    "installable": True
+    "installable": True,
+    "auto_install": False,
 }
--- stock_lot_notes/models/lot.py
+++ stock_lot_notes/models/lot.py
@@ -2,14 +2,14 @@
 ##############################################################################
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see http://www.gnu.org/licenses/.
@@ -19,13 +19,7 @@
 from openerp import models, fields
 
 
-class MrpBom(models.Model):
-    _inherit = 'mrp.bom'
+class StockProductionLot(models.Model):
+    _inherit = 'stock.production.lot'
 
     notes = fields.Html()
-
-
-class MrpProduction(models.Model):
-    _inherit = 'mrp.production'
-
-    bom_notes = fields.Html(string='BoM Notes', related='bom_id.notes')
