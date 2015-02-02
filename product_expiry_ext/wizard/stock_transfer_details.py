c076ac02ddb57160ae8bb43b2249d01f371ed7e5
oihane <oihanecruce@gmail.com>
2015-02-02 14:46:52 +0100

    [NEW] Moved module <product_expiry_ext> from wip

---
+++ product_expiry_ext/wizard/stock_transfer_details.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+
+from openerp import fields, models
+
+
+class TransferDetailsItems(models.TransientModel):
+    _inherit = 'stock.transfer_details_items'
+
+    life_date = fields.Datetime(string='Life Date', related='lot_id.life_date')
