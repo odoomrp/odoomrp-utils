01c5547ed7c93496e87c23b0419a6147609fddda
oihane <oihanecruce@gmail.com>
2015-02-04 12:57:08 +0100

    [ADD] New module <stock_quant_stock_info>

---
+++ stock_quant_stock_info/models/stock_quant.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+
+from openerp import models, fields
+
+
+class StockQuant(models.Model):
+    _inherit = 'stock.quant'
+
+    product_incoming_qty = fields.Float(
+        string='Incoming Product', related='product_id.incoming_qty',
+        help="Quantity of products that are planned to arrive.\n"
+             "In a context with a single Stock Location, this includes "
+             "goods arriving to this Location, or any of its children.\n"
+             "In a context with a single Warehouse, this includes "
+             "goods arriving to the Stock Location of this Warehouse, or "
+             "any of its children.\n"
+             "Otherwise, this includes goods arriving to any Stock "
+             "Location with 'internal' type.")
+    product_outgoing_qty = fields.Float(
+        string='Outgoing Product', related='product_id.outgoing_qty',
+        help="Quantity of products that are planned to leave.\n"
+             "In a context with a single Stock Location, this includes "
+             "goods leaving this Location, or any of its children.\n"
+             "In a context with a single Warehouse, this includes "
+             "goods leaving the Stock Location of this Warehouse, or "
+             "any of its children.\n"
+             "Otherwise, this includes goods leaving any Stock "
+             "Location with 'internal' type.")
