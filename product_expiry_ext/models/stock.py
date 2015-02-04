11f9b15dd0e1f3932150e9c4425d07bd92654a9f
oihane <oihanecruce@gmail.com>
2015-02-04 10:06:15 +0100

    [MOD] Deleting store=True option

--- product_expiry_ext/models/stock.py
+++ product_expiry_ext/models/stock.py
@@ -45,7 +45,7 @@ class StockProductioLot(models.Model):
                    ('normal', 'Normal'),
                    ('to_remove', 'To remove'),
                    ('best_before', 'After the best before')],
-        string='Expiry state', store=True)
+        string='Expiry state')
 
 
 class StockQuant(models.Model):
