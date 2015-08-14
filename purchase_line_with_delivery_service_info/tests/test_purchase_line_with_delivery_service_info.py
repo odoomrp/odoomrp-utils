183f6e8bfa22141e3e047242f72743a725cf26bb
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2015-08-14 09:47:23 +0200

    [FIX] purchase_line_with_delivery_service_info: Fix test error.

--- purchase_line_with_delivery_service_info/tests/test_purchase_line_with_delivery_service_info.py
+++ purchase_line_with_delivery_service_info/tests/test_purchase_line_with_delivery_service_info.py
@@ -9,60 +9,58 @@ class TestPurchaseLineWithDeliveryServiceInfo(common.TransactionCase):
 
     def setUp(self):
         super(TestPurchaseLineWithDeliveryServiceInfo, self).setUp()
-        self.route_model = self.env['stock.location.route']
-        self.product_model = self.env['product.product']
         self.sale_model = self.env['sale.order']
-        self.sale_line_model = self.env['sale.order.line']
         self.procurement_model = self.env['procurement.order']
-        routes = self.route_model.search(
-            [('name', 'in', ('Make To Order', 'Buy'))])
-        self.assertEqual(len(routes), 2,
-                         "Make to order, and buy routes not found")
-        vals = self.sale_model.onchange_partner_id(
-            self.env.ref('base.res_partner_1').id).get('value')
-        line = self.sale_line_model.product_id_change(
-            pricelist=vals.get('pricelist_id'),
-            product=self.env.ref('product.product_product_6').id, qty=1,
-            qty_uos=1,
-            partner_id=self.env.ref('base.res_partner_1').id).get('value')
-        line['product_id'] = self.env.ref('product.product_product_6').id
-        vals.update({'partner_id': self.env.ref('base.res_partner_1').id,
-                     'order_line': [(0, 0, line)],
-                     'carrier_id':
-                     self.env.ref('delivery.normal_delivery_carrier').id})
-        self.new_sale_order = self.sale_model.create(vals)
-        self.new_sale_order.delivery_set()
-        for line in self.new_sale_order.order_line:
+        sale_vals = {
+            'partner_id': self.env.ref('base.res_partner_1').id,
+            'partner_shipping_id': self.env.ref('base.res_partner_1').id,
+            'partner_invoice_id': self.env.ref('base.res_partner_1').id,
+            'pricelist_id': self.env.ref('product.list0').id,
+            'carrier_id': self.env.ref('delivery.normal_delivery_carrier').id}
+        sale_line_vals = {
+            'product_id': self.env.ref('product.product_product_6').id,
+            'name': self.env.ref('product.product_product_6').name,
+            'product_uos_qty': 1,
+            'product_uom': self.env.ref('product.product_product_6').uom_id.id,
+            'price_unit': self.env.ref('product.product_product_6').list_price}
+        sale_vals['order_line'] = [(0, 0, sale_line_vals)]
+        self.sale_order = self.sale_model.create(sale_vals)
+        self.sale_order.delivery_set()
+        for line in self.sale_order.order_line:
             if line.product_id.type == 'service':
                 line.product_id.write(
-                    {'route_ids': [(6, 0, routes.mapped('id'))],
+                    {'route_ids':
+                     [(6, 0,
+                       [self.env.ref('stock.route_warehouse0_mto').id,
+                        self.env.ref('purchase.route_warehouse0_buy').id])],
                      'seller_ids':
                      [(6, 0, [self.env.ref('base.res_partner_14').id])]})
+                self.service_product = line.product_id
                 line.write({'delivery_standard_price': 578.00})
 
     def test_confirm_sale_with_delivery_service(self):
-        self.new_sale_order.action_button_confirm()
-        for line in self.new_sale_order.order_line:
+        self.sale_order.action_button_confirm()
+        cond = [('origin', '=', self.sale_order.name),
+                ('product_id', '=', self.service_product.id)]
+        procurement = self.procurement_model.search(cond)
+        self.assertEqual(
+            len(procurement), 1,
+            "Procurement not generated for the service product type")
+        procurement.run()
+        cond = [('group_id', '=', procurement.group_id.id),
+                ('product_id', '=', self.service_product.id),
+                ('state', '=', 'confirmed')]
+        procurement2 = self.procurement_model.search(cond)
+        self.assertEqual(
+            len(procurement2), 1,
+            "Procurement2 not generated for the service product type")
+        procurement2.run()
+        self.assertTrue(
+            bool(procurement2.purchase_id),
+            "Purchase no generated for procurement Service")
+        for line in procurement2.purchase_id.order_line:
             if line.product_id.type == 'service':
-                cond = [('sale_line_id', '=', line.id)]
-                procurement = self.procurement_model.search(cond)
                 self.assertEqual(
-                    len(procurement), 1,
-                    "Procurement not generated for the service product type")
-                procurement.run()
-                cond = [('id', '>', procurement.id),
-                        ('sale_line_id', '=', line.id)]
-                procurement2 = self.procurement_model.search(cond)
-                self.assertEqual(
-                    len(procurement2), 1,
-                    "Procurement2 not generated for the service product type")
-                procurement2.run()
-                self.assertTrue(
-                    bool(procurement2.purchase_id),
-                    "Purchase no generated for procurement Service")
-                for line in procurement2.purchase_id.order_line:
-                    if line.product_id.type == 'service':
-                        self.assertEqual(
-                            line.price_unit,
-                            procurement2.sale_line_id.delivery_standard_price,
-                            "Erroneous price on purchase order line")
+                    line.price_unit,
+                    procurement2.sale_line_id.delivery_standard_price,
+                    "Erroneous price on purchase order line")
