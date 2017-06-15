8cbc1b86e696bde8acd2fdf196c9311354b980f2
oihane <oihanecruce@gmail.com>
2017-06-15 17:21:44 +0200

    [IMP] change dependecies

--- stock_planning_mrp/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from . import stock_planning
-from . import mrp_production
-from . import procurement_order
--- stock_planning_procurement_generated_by_plan/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from . import stock_planning
-from . import procurement_order
-from . import stock_move
-from . import stock_reservation
